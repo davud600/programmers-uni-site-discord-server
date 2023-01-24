e0c536ce8da2ffb1f396fa3c30b5c0ad2a35d03a
davud <davudgorqi@gmail.com>
2023-01-24 11:36:46 +0100

    fix: check due payments and update roles intervals, and cleaned some code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
 import { poolPromise } from './databases';
 import { Member } from './interfaces/members.interface';
-import { loginClient, upgradeRole } from './utils/discord';
+import {
+  downgradeRole,
+  loginClient,
+  upgradeRole,
+  getServerMember,
+} from './utils/discord';
 
 const MILLISECONDS_IN_HOUR = 3600000;
 const DUE_PAYMENTS_TIME_INTERVAL_HOURS = 23;
-const UPDATE_ROLES_TIME_INTERVAL_HOURS = 0.25; // 15 minutes
-const MAX_DAYS_WITHOUT_PAYING = 28;
+const UPDATE_ROLES_TIME_INTERVAL_HOURS = 0.1667; // 10 minutes
+const MAX_DAYS_WITHOUT_PAYING_WARNING = 28;
+const MAX_DAYS_WITHOUT_PAYING = 32;
 
 const testing_interval_time = 10000;
 
-async function onMemberDuePayment(member: Member) {
+function isMember(discordUsername: string, serverMember: any): boolean {
+  if (!serverMember) {
+    console.log(
+      `Member with username: ${discordUsername}, does not exist on our discord server`,
+    );
+
+    return false;
+  }
+
+  return true;
+}
+
+async function warnMember(member: Member) {
+  const serverMember = await getServerMember(member.discord_username);
+  if (!isMember(member.discord_username, serverMember)) return;
+
   // message them on discord
   // message owners on discord
 
@@ -18,25 +39,50 @@ async function onMemberDuePayment(member: Member) {
   }'`;
 
   await poolPromise.execute(sql);
+
+  console.log(`Warned ${member.discord_username} about payment`);
+}
+
+async function removeMember(member: Member) {
+  const serverMember = await getServerMember(member.discord_username);
+  if (!isMember(member.discord_username, serverMember)) return;
+
+  const sql = `UPDATE members SET warned_about_payment='${0}', is_member='${0}' WHERE id='${
+    member.id
+  }'`;
+
+  await poolPromise.execute(sql);
+  await downgradeRole(serverMember);
 }
 
 async function checkDuePayments() {
-  const sql = `SELECT * FROM members 
-WHERE last_paid < DATE_SUB(CURDATE(), INTERVAL ${MAX_DAYS_WITHOUT_PAYING} DAY)`;
+  const sqlWarnings = `SELECT * FROM members 
+WHERE last_paid < DATE_SUB(CURDATE(), INTERVAL ${MAX_DAYS_WITHOUT_PAYING_WARNING} DAY) AND warned_about_payment='${0}' AND is_member='${1}'`;
+  const sqlRemove = `SELECT * FROM members 
+WHERE last_paid < DATE_SUB(CURDATE(), INTERVAL ${MAX_DAYS_WITHOUT_PAYING} DAY) AND warned_about_payment='${1}' AND is_member='${1}'`;
 
-  const [members]: Array<Array<Member>> = await poolPromise.execute(sql);
+  const [membersToWarn]: Array<Array<Member>> = await poolPromise.execute(
+    sqlWarnings,
+  );
 
-  members.forEach(async (member: Member) => await onMemberDuePayment(member));
+  const [membersToRemove]: Array<Array<Member>> = await poolPromise.execute(
+    sqlRemove,
+  );
+
+  membersToWarn.forEach(async (member: Member) => await warnMember(member));
+  membersToRemove.forEach(async (member: Member) => await removeMember(member));
 }
 
 async function updateRoles() {
-  const sql = `SELECT * FROM members WHERE is_member='${1}'`;
+  const sql = `SELECT * FROM members WHERE last_paid > DATE_SUB(CURDATE(), INTERVAL ${MAX_DAYS_WITHOUT_PAYING_WARNING} DAY) AND is_member='${1}'`;
 
   const [members]: Array<Array<Member>> = await poolPromise.execute(sql);
 
   members.forEach(async (member: Member) => {
-    // change their role using discord api if they arent member already
-    upgradeRole(member.discord_username);
+    const serverMember = await getServerMember(member.discord_username);
+    if (!isMember(member.discord_username, serverMember)) return;
+
+    upgradeRole(serverMember);
   });
 }
 
@@ -45,8 +91,8 @@ function main() {
 
   setInterval(
     checkDuePayments,
-    DUE_PAYMENTS_TIME_INTERVAL_HOURS * MILLISECONDS_IN_HOUR,
-    // testing_interval_time,
+    // DUE_PAYMENTS_TIME_INTERVAL_HOURS * MILLISECONDS_IN_HOUR,
+    testing_interval_time,
   );
   setInterval(
     updateRoles,
--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -4,8 +4,6 @@ import {
   MEMBER_ROLE_ID,
 } from '../config/index';
 import discord from 'discord.js';
-import { whitelist } from '../config/whitelist.json';
-// import blacklist from '../config/blacklist.json';
 
 const client = new discord.Client({
   intents: [
@@ -14,45 +12,50 @@ const client = new discord.Client({
   ],
 });
 
+let guild: any;
+
 client.on('ready', async () => {
   console.log(`Logged in as ${client?.user?.tag}!`);
+
+  guild = client.guilds.cache.get(DISCORD_GUILD_ID ?? '');
 });
 
+const getServerMembers = async () => await guild.members.fetch();
+
+const getServerMember = async (discordUsername: string) =>
+  getServerMembers().then((members: any[]) =>
+    members.find(member => member.user.tag === discordUsername),
+  );
+
 const changeMemberRole = async (
-  memberUsername: string,
+  member: any,
   ROLE_ID: any,
   upgrading: boolean = true,
 ) => {
-  const guild: any = client.guilds.cache.get(DISCORD_GUILD_ID ?? '');
-
-  const members: any[] = await guild.members.fetch();
-
-  const member: any = members.find(
-    member => member.user.tag === memberUsername,
-  );
-
-  if (!member) {
-    console.log(
-      `Member with username: ${memberUsername}, was not found in the server!`,
-    );
-    return;
-  }
-
   if (member.roles.cache.has(ROLE_ID)) {
-    console.log(`${member.user.tag} is already a Member`);
+    if (upgrading) {
+      console.log(`${member.user.tag} is already a Member`);
+      return;
+    }
+
+    member.roles.remove(ROLE_ID);
+    console.log(`${member.user.tag} just got their Member role removed`);
     return;
   }
 
-  member.roles.add(ROLE_ID);
-  console.log(`${member.user.tag} just became a Member`);
+  if (upgrading) {
+    member.roles.add(ROLE_ID);
+    console.log(`${member.user.tag} just became a Member`);
+    return;
+  }
 };
 
 const loginClient = () => client.login(DISCORD_BOT_TOKEN);
 
-const upgradeRole = (memberUsername: string) =>
-  changeMemberRole(memberUsername, MEMBER_ROLE_ID, true);
+const upgradeRole = (member: any) =>
+  changeMemberRole(member, MEMBER_ROLE_ID, true);
 
-const downgradeRole = (memberUsername: string) =>
-  changeMemberRole(memberUsername, MEMBER_ROLE_ID, false);
+const downgradeRole = (member: any) =>
+  changeMemberRole(member, MEMBER_ROLE_ID, false);
 
-export { loginClient, upgradeRole, downgradeRole };
+export { loginClient, upgradeRole, downgradeRole, getServerMember };
