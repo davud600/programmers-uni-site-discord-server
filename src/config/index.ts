bf0b5e4c18702bfc1a3d63bfcc360444a884aaf6
davud <davudgorqi@gmail.com>
2023-02-10 00:14:08 +0100

    chore: removed unused db env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,11 +3,6 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const {
-  DB_HOST,
-  DB_USER,
-  DB_DATABASE,
-  DB_PASSWORD,
-  DB_PORT,
   DISCORD_BOT_TOKEN,
   DISCORD_GUILD_ID,
   MEMBER_ROLE_ID,
